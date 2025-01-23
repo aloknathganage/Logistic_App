f41615967bde486ac778a416336ae6c16040d896
Aloknath Ganage <69706421+aloknathganage@users.noreply.github.com>
2025-01-23 20:56:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'Templates')));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Templates', 'index.html'));
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
